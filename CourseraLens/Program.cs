c239ce0f29553cd67ef16447a36a2c843913d1c2
jacinyan <jacinjiyan@gmail.com>
2025-04-10 03:31:09 +1000

    Add database context and SQL Server dependencies

--- CourseraLens/Program.cs
+++ CourseraLens/Program.cs
@@ -1,25 +1,65 @@
+using CourseraLens.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// ===== Services =====
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cors
+builder.Services.AddCors(options =>
+{
+    // Default
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]!);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+
+    // Custom 
+    options.AddPolicy("AnyOrigin", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+});
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// ===== Build =====
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
 
+app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthorization();
 
+// ===== Endpoints  =====
+app.MapGet("/error",
+    [EnableCors("AnyOrigin")] [ResponseCache(NoStore = true)]
+    () =>
+        Results.Problem());
 app.MapControllers();
 
 app.Run();
