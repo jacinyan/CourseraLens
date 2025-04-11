757305cd33f5872c14df9fe3edd281122a16bd84
jacinyan <jacinjiyan@gmail.com>
2025-04-12 05:10:42 +1000

    Prepare to refactor CoursesController using Redis

--- CourseraLens/Controllers/TagsController.cs
+++ CourseraLens/Controllers/TagsController.cs
@@ -1,10 +1,12 @@
 using System.Diagnostics;
 using System.Linq.Dynamic.Core;
+using System.Text.Json;
 using CourseraLens.Attributes;
 using CourseraLens.DTO;
 using CourseraLens.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CourseraLens.Controllers;
 
@@ -13,42 +15,36 @@ namespace CourseraLens.Controllers;
 public class TagsController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
-    private readonly ILogger<CoursesController> _logger;
+    private readonly ILogger<TagsController> _logger;
+    private readonly IMemoryCache _memoryCache;
 
-    public TagsController(ApplicationDbContext context,
-        ILogger<CoursesController> logger)
+    public TagsController(
+        ApplicationDbContext context,
+        ILogger<TagsController> logger,
+        IMemoryCache memoryCache)
     {
         _context = context;
         _logger = logger;
+        _memoryCache = memoryCache;
     }
 
     [HttpGet(Name = "GetTags")]
     [ResponseCache(CacheProfileName = "Any-60")]
     [ManualValidationFilter]
     public async Task<ActionResult<RestDto<Tag[]>>> Get(
-        [FromQuery] RequestDto<TagDto> input
-    )
+        [FromQuery] RequestDto<TagDto> input)
     {
-        // Customize ModelState 
         if (!ModelState.IsValid)
         {
             var details = new ValidationProblemDetails(ModelState);
-            details.Extensions["traceId"] =
-                Activity.Current?.Id
-                ?? HttpContext.TraceIdentifier;
+            details.Extensions["traceId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
             if (ModelState.Keys.Any(k => k == "PageSize"))
             {
-                details.Type =
-                    "https://tools.ietf.org/html/rfc7231#section-6.6.2";
+                details.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.2";
                 details.Status = StatusCodes.Status501NotImplemented;
-                return new ObjectResult(details)
-                {
-                    StatusCode = StatusCodes.Status501NotImplemented
-                };
+                return new ObjectResult(details) { StatusCode = StatusCodes.Status501NotImplemented };
             }
-
-            details.Type =
-                "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+            details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
             details.Status = StatusCodes.Status400BadRequest;
             return new BadRequestObjectResult(details);
         }
@@ -57,14 +53,23 @@ public class TagsController : ControllerBase
         if (!string.IsNullOrEmpty(input.FilterQuery))
             query = query.Where(b => b.TagName.Contains(input.FilterQuery));
         var resultCount = await query.CountAsync();
-        query = query
-            .OrderBy($"{input.SortColumn} {input.SortOrder}")
-            .Skip(input.PageIndex * input.PageSize)
-            .Take(input.PageSize);
 
+        Tag[]? result = null;
+        var cacheKey = $"{input.GetType()} - {JsonSerializer.Serialize(input)}";
+        if (!_memoryCache.TryGetValue<Tag[]>(cacheKey, out result))
+        {
+            query = query
+                .OrderBy($"{input.SortColumn} {input.SortOrder}")
+                .Skip(input.PageIndex * input.PageSize)
+                .Take(input.PageSize);
+            
+            result = await query.ToArrayAsync();
+            _memoryCache.Set(cacheKey, result, new TimeSpan(0, 0, 30));
+        }
+        
         return new RestDto<Tag[]>
         {
-            Data = await query.ToArrayAsync(),
+            Data = result,
             PageIndex = input.PageIndex,
             PageSize = input.PageSize,
             ResultCount = resultCount,
@@ -73,7 +78,9 @@ public class TagsController : ControllerBase
                 new(
                     Url.Action(null, "Tags",
                         new { input.PageIndex, input.PageSize },
-                        Request.Scheme)!, "self", "GET")
+                        Request.Scheme)!,
+                    "self",
+                    "GET")
             }
         };
     }
